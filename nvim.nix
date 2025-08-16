312e76f7fb14e1b5e8abc634c181abd10ce3ba83
svinoshpatel <shpakmark@gmail.com>
2025-08-16 07:25:33 +0300

    Home manager rebuild

--- nvim.nix
+++ nvim.nix
@@ -4,19 +4,19 @@
 
     settings = {
       number = true;
-      relativenumber = true;
-      termguicolors = true;
-      signcolumn = "yes";
-      scrolloff = 999;
-
-      tabstop = 4;
-      shiftwidth = 4;
-      softtabstop = 4;
-      smartindent = true;
-
-        ignorecase = true;
-      smartcase = true;
-      incsearch = true;
+      # relativenumber = true;
+      # termguicolors = true;
+      # signcolumn = "yes";
+      # scrolloff = 999;
+      #
+      # tabstop = 4;
+      # shiftwidth = 4;
+      # softtabstop = 4;
+      # smartindent = true;
+      #
+      #   ignorecase = true;
+      # smartcase = true;
+      # incsearch = true;
 
       vim = {
             clipboard.enable = true;
