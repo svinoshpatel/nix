57872ef464d62ab2b919be77c50d6d5c1faeb78e
svinoshpatel <shpakmark@gmail.com>
2025-08-16 07:52:12 +0300

    Home manager rebuild

--- nvim.nix
+++ nvim.nix
@@ -1,42 +1,45 @@
 {
-    programs.nvf = {
-    enable = true;
+	programs.nvf = {
+		enable = true;
 
-    settings = {
+		settings = {
+			vim = {
+				clipboard.enable = true;
 
-      vim = {
-            clipboard.enable = true;
+				theme = {
+					enable = true;
+					name = "catppuccin";
+					style = "mocha";
+				};
 
-        theme = {
-                enable = true;
-                name = "catppuccin";
-                style = "mocha";
-        };
+				languages = {
+					enableLSP = true;
+					enableTreesitter = true;
 
-        languages = {
-              nix.enable = true;
-              clang.enable = true;
-              ts.enable = true;
-              html.enable = true;          
-        };
-        options = {
-      number = true;
-      relativenumber = true;
-      termguicolors = true;
-      signcolumn = "yes";
-      scrolloff = 999;
+					nix.enable = true;
+					clang.enable = true;
+					ts.enable = true;
+					html.enable = true;          
+				};
+				
+				options = {
+					number = true;
+					relativenumber = true;
+					termguicolors = true;
+					signcolumn = "yes";
+					scrolloff = 999;
 
-      tabstop = 4;
-      shiftwidth = 4;
-        softtabstop = 0;
-        smartindent = true;
-        expandtab = false;
+					tabstop = 4;
+					shiftwidth = 4;
+					softtabstop = 0;
+					smartindent = true;
+					expandtab = false;
 
-        ignorecase = true;
-        smartcase = true;
-        incsearch = true;
-        };
-      };
-    };
-  };
+					ignorecase = true;
+					smartcase = true;
+					incsearch = true;
+				};
+			};
+		};
+	};
 }
