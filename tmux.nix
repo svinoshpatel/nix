4e3b3ac7764d84c3f9adb5eea5f69c8054347fa0
svinoshpatel <shpakmark@gmail.com>
2025-08-17 15:47:04 +0300

    Home manager rebuild

--- tmux.nix
+++ tmux.nix
@@ -5,7 +5,7 @@
 		clock24 = true;
 		escapeTime = 0;
 		keyMode = "vi";
-		prefix = "C-a";
+		# prefix = "C-a";
 		shell = "/run/current-system/sw/bin/fish";
 		shortcut = "a";
 		terminal = "screen-256color";
